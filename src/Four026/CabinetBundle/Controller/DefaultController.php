a1f428537ce6091b9afe1a902abbf58a8c04fee5
4026 <name4026+github@gmail.com>
2014-11-26 19:46:09 +0000

    Adding basic security config.

--- src/Four026/CabinetBundle/Controller/DefaultController.php
+++ src/Four026/CabinetBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Four026\CabinetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class DefaultController extends Controller
 {
@@ -10,6 +12,36 @@ class DefaultController extends Controller
     {
         return $this->render('Four026CabinetBundle:Default:cover.html.twig');
     }
+
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+
+        return $this->render(
+            'Four026CabinetBundle:Security:login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
 
     public function listDocumentsAction($character_name)
     {

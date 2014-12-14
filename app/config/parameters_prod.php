9506ca9eb28f456f46b3186b570d54226d0befda
Ben Hallett <name4026@gmail.com>
2014-12-14 17:41:37 +0000

    Setting up configuration for AWS RDS DBs.

--- app/config/parameters_prod.php
+++ app/config/parameters_prod.php
@@ -3,6 +3,33 @@
  * Application parameters for the prod environment (That is, AWS).
  */
 
+if (php_sapi_name() == 'cli')
+{
+    /* In the CLI (used by the Symfony app/console script to perform setup & maintenance tasks), RDS environment variables
+     * aren't set automatically, so we're going to import them manually.
+     *
+     * This approach lifted from this SO answer: http://stackoverflow.com/a/23936080
+     */
+
+    $env_vars_file = "/opt/elasticbeanstalk/support/envvars"; //This seems to be where Elastic Beanstalk stores the RDS env vars.
+
+    if (file_exists($env_vars_file) && is_file($env_vars_file)) {
+        $contents = file_get_contents($env_vars_file);
+        foreach(explode("\n", $contents) as $line) {
+            if (empty($line)) continue;
+
+            $new_line = str_replace('export ', '', $line);
+            $first_part = strpos($new_line, '=');
+
+            $last_part = substr($new_line, $first_part, strlen($new_line));
+            $variable_value = str_replace(array('=', '"'), array('',''), $last_part);
+
+            $variable_name = substr($new_line, 0, $first_part);
+            putenv($variable_name."=".$variable_value);
+        }
+    }
+}
+
 $container->setParameter('database_driver', 'pdo_mysql');
 $container->setParameter('database_host', $_SERVER['RDS_HOSTNAME']);
 $container->setParameter('database_port', $_SERVER['RDS_PORT']);
